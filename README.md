422006c6e6a9019c1ba1f8bc6e733c22dbd45407
bryznnguyen <bryznnguyen@gmail.com>
2025-09-12 18:38:43 -0500

    git ignore update

--- README.md
+++ README.md
@@ -3,3 +3,39 @@
 #                   NOTIFICATION-SERVICE                       #
 #                                                              #
 ################################################################
+
+{
+  "topicName": "LoginResponse",
+  "correlatorId": 12345,
+  "status": "SUCCESSFUL",
+  "username": "bnguyen",
+  "timestamp": "2025-09-12T22:15:30Z"
+}
+
+{
+  "topicName": "RewardsResponse",
+  "correlatorId": 98765,
+  "email": "john.doe@example.com",
+  "username": "johndoe",
+  "rewardPoints": 1200,
+  "application": "SUCCESS",
+  "timestamp": "2025-09-12T22:30:45Z"
+}
+
+{
+  "topicName": "NewAccountResponse",
+  "correlatorId": 45678,
+  "username": "new_user123",
+  "status": "SUCCESSFUL",
+  "statusMessage": "Account created successfully"
+}
+
+{
+  "topicName": "PaymentResponse",
+  "correlatorId": 20001,
+  "paymentAmount": 149.99,
+  "email": "customer.one@example.com",
+  "creditCard": "4111111111111111",
+  "timestamp": "2025-09-12T23:00:00Z",
+  "status": "SUCCESSFUL"
+}
---
+++ src/main/java/com/SpringBootConsoleApplication.java
@@ -0,0 +1,34 @@
+package com;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class SpringBootConsoleApplication
+        implements CommandLineRunner {
+
+    private static Logger LOG = LoggerFactory
+            .getLogger(SpringBootConsoleApplication.class);
+
+    public static void main(String[] args) {
+        LOG.info("STARTING THE [NOTIFICATION-SERVICE]");
+        SpringApplication.run(SpringBootConsoleApplication.class, args);
+        LOG.info("[NOTIFICATION-SERVICE] FINISHED STARTING UP");
+    }
+
+    @Override
+    public void run(String... args) {
+        LOG.info("EXECUTING : command line runner");
+
+        for (int i = 0; i < args.length; ++i) {
+            LOG.info("args[{}]: {}", i, args[i]);
+        }
+    }
+}
